fb226034a8e0d945c18b73d66eeac5cb8f30e14e
ParadoxAI-Co <paradoxaico@gmail.com>
2023-12-13 23:08:24 +0330

    Add files via upload

---
+++ ag/__init__.py
@@ -0,0 +1,97 @@
+# Import required modules
+import cv2 as cv
+import math
+import time
+import argparse
+import random
+import os
+def recognize(path, model_path):
+    def getFaceBox(net, frame, conf_threshold=0.7):
+        frameOpencvDnn = frame.copy()
+        frameHeight = frameOpencvDnn.shape[0]
+        frameWidth = frameOpencvDnn.shape[1]
+        blob = cv.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+
+        net.setInput(blob)
+        detections = net.forward()
+        bboxes = []
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > conf_threshold:
+                x1 = int(detections[0, 0, i, 3] * frameWidth)
+                y1 = int(detections[0, 0, i, 4] * frameHeight)
+                x2 = int(detections[0, 0, i, 5] * frameWidth)
+                y2 = int(detections[0, 0, i, 6] * frameHeight)
+                bboxes.append([x1, y1, x2, y2])
+                cv.rectangle(frameOpencvDnn, (x1, y1), (x2, y2), (0, 255, 0), int(round(frameHeight/150)), 8)
+        return frameOpencvDnn, bboxes
+
+    faceProto = model_path + "/opencv_face_detector.pbtxt"
+    faceModel = model_path + "/opencv_face_detector_uint8.pb"
+
+    ageProto = model_path + "/age_deploy.prototxt"
+    ageModel = model_path + "/age_net.caffemodel"
+
+    genderProto = model_path + "/gender_deploy.prototxt"
+    genderModel = model_path + "/gender_net.caffemodel"
+
+    MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+    ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+    genderList = ['Male', 'Female']
+
+
+    # Load network
+    ageNet = cv.dnn.readNet(ageModel, ageProto)
+    genderNet = cv.dnn.readNet(genderModel, genderProto)
+    faceNet = cv.dnn.readNet(faceModel, faceProto)
+
+    # Open a video file or an image file or a camera stream
+    cap = cv.VideoCapture(path)
+    padding = 20
+    while cv.waitKey(1) < 0:
+        # Read frame
+        t = time.time()
+        hasFrame, frame = cap.read()
+        if not hasFrame:
+            cv.waitKey()
+            break
+
+        frameFace, bboxes = getFaceBox(faceNet, frame)
+        if not bboxes:
+            print("No face Detected, Checking next frame")
+            continue
+
+        for bbox in bboxes:
+            # print(bbox)
+            face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+
+            blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+            genderNet.setInput(blob)
+            genderPreds = genderNet.forward()
+            gender = genderList[genderPreds[0].argmax()]
+
+
+            ageNet.setInput(blob)
+            agePreds = ageNet.forward()
+            age = ageList[agePreds[0].argmax()]
+            print("Gender : {}".format(gender))
+            print("Age : {}".format(age))
+            print("********************************************************************************")
+            label = "{},{}".format(gender, age)
+            cv.putText(frameFace, label, (bbox[0], bbox[1]-10), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 255), 2, cv.LINE_AA)
+            cv.imshow("age & gender", frameFace)
+        with open("age & gender {}.txt".format(random.random()), 'w') as f:
+            for bbox in bboxes:
+                # print(bbox)
+                face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+    
+                blob = cv.dnn.blobFromImage(face, 1.0, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
+                genderNet.setInput(blob)
+                genderPreds = genderNet.forward()
+                gender = genderList[genderPreds[0].argmax()]
+    
+    
+                ageNet.setInput(blob)
+                agePreds = ageNet.forward()
+                age = ageList[agePreds[0].argmax()]
+                f.write("Age: {} Gender: {} \n".format(age, gender))
